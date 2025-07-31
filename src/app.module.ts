61876bac40887aeaff9e88486b7feb0bf5734255
Ricky Ultimate <rickyogigi@gmail.com>
2025-07-31 14:58:25 +0100

    feat: create prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FragmentsModule } from './fragments/fragments.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, FragmentsModule],
+  imports: [UsersModule, FragmentsModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
